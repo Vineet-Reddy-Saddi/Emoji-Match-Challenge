341b71425b8e61dfeab6071c5b199e87085a3633
Tanisi Reddy <reddytanisi@gmail.com>
2025-10-12 17:16:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2267796123-7d8c9",
+  "appId": "1:450094252193:web:ce37120eb1753fa14630e0",
+  "apiKey": "AIzaSyB2b6YLy3SxNR6AF_KQ0yPtSyNRgnA6AEE",
+  "authDomain": "studio-2267796123-7d8c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "450094252193"
+};
